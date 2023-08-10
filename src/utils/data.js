4a9b8be98d8f8d510cfa469122a472f2f2e0fafc
Yassine Oujama <yassine.oujama@gmail.com>
2023-08-10 12:03:52 +0100

    react-Y4x-portfolio

---
+++ src/utils/data.js
@@ -0,0 +1,144 @@
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { CiMobile1 } from "react-icons/ci";
+import { MdWorkspacesOutline } from "react-icons/md";
+export const projectExperience = [
+  {
+    name: "React.js",
+    projects: 76,
+    icon: HiOutlineDesktopComputer,
+    bg: "#286F6C",
+  },
+  {
+    name: "Node.js",
+    projects: 76,
+    icon: HiOutlineDesktopComputer,
+    bg: "#286F6C",
+  },
+  {
+    name: "Laravel",
+    projects: 63,
+    icon: CiMobile1,
+    bg: "#EEC048",
+  },
+  {
+    name: "Php",
+    projects: 63,
+    icon: CiMobile1,
+    bg: "#EEC048",
+  },
+  {
+    name: "JavaScript",
+    projects: 63,
+    icon: CiMobile1,
+    bg: "#EEC048",
+  },
+  {
+    name: "Python",
+    projects: 63,
+    icon: CiMobile1,
+    bg: "#EEC048",
+  },
+  {
+    name: "MongoDB",
+    projects: 47,
+    icon: MdWorkspacesOutline,
+    bg: "#F26440",
+  },
+  {
+    name: "MySql | SqlServer",
+    projects: 47,
+    icon: MdWorkspacesOutline,
+    bg: "#F26440",
+  },
+  {
+    name: "Html + Css + Bootstrap",
+    projects: 47,
+    icon: MdWorkspacesOutline,
+    bg: "#F26440",
+  },
+];
+
+export const WhatDoIHelp = [
+  "I am a web developer who loves to create beautiful and simple things. I believe that technology should be easy to use and understand, and I take pride in creating user experiences that are both functional and aesthetically pleasing.",
+];
+
+export const workExp = [
+  {
+    place: "ALX Software Engineering.",
+    tenure: "Aug 2023 - Sep 2024",
+    role: "Check Printer Pro | Majjane",
+    detail:
+      "Discover our web application to manage and print your bank checks and letters of credit. Simplify your financial transactions by automating the process of creation and printing. Save time and ensure the compliance made easy",
+  },
+  {
+    place: "Digital Development Diploma, Option Web Full stack.",
+    tenure: "Aug 2021 - Sep 2023",
+    role: "Weather website",
+    detail:
+      "A weather site developed with React can offer several features such as current weather display, forecasts, weather maps, By using these features, users can obtain precise and real-time information on the current weather conditions",
+  },
+  {
+    place: "International Baccalaureate in Experimental Sciences, Physical Sciences option",
+    tenure: "Aug 2020 - Sep 2021",
+    role: "E-commerce web site | PlanetPhone",
+    detail:
+      "E-commerce site developed with Laravel, Key features include a management system for products, a shopping cart, an ordering system, a user management, and order management",
+  },
+];
+
+export const comments = [
+  {
+    title: "I develop beautiful simple things",
+    content:
+      "I am a passionate web developer who loves to create beautiful and simple things. I am proficient in a variety of programming languages, including HTML, CSS, JavaScript, and Python. I also have experience with a variety of software development tools, such as Git, GitHub, and Jira.",
+  },
+  {
+    title: "I believe in technology for good",
+    post: "Creative Manager",
+    content:
+      "I believe that technology should be used to make people's lives better. I am always looking for ways to use my skills to create innovative and user-friendly solutions to real-world problems.",
+  },
+  {
+    title: "I am always learning",
+    content:
+      "I am passionate about technology and I am always looking for ways to learn new things. I am a quick learner and I am always up for a challenge. I am also a good communicator and I am able to clearly explain complex technical concepts to both technical and non-technical audiences.",
+  },
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
